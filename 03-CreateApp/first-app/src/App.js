ce832008543fc1a279a7952b8092b276dae870f8
Arlen Neylon <aneylon@scramsystems.com>
2023-05-15 18:55:30 -0600

    State and condititional rendering.

--- 03-CreateApp/first-app/src/App.js
+++ 03-CreateApp/first-app/src/App.js
@@ -5,15 +5,59 @@ import { useState } from "react";
 function App() {
   // let name = "dude";
   const [name, setName] = useState("dude");
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "one", id: 1 },
+    { title: "two", id: 2 },
+    { title: "three", id: 3 },
+  ]);
   const changeName = () => {
     // name = "bro";
     setName("bro");
     console.log(name);
   };
+
+  const deleteEvent = (id) => {
+    console.log("delete : ", id);
+    // possible to be out of sync with event updates.
+    // let filtered = events.filter((event) => {
+    //   if (event.id !== id) return event;
+    // });
+    setEvents((previous) => {
+      return previous.filter((event) => {
+        return event.id !== id;
+      });
+    });
+  };
   return (
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={changeName}>Change name</button>
+      <div>
+        {showEvents && (
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        )}
+        {!showEvents && (
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        )}
+      </div>
+      {showEvents &&
+        events.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <h2>
+                {index + 1} : {item.title}
+              </h2>
+              <button
+                onClick={() => {
+                  deleteEvent(item.id);
+                }}
+              >
+                delete event
+              </button>
+            </div>
+          );
+        })}
     </div>
   );
 }
