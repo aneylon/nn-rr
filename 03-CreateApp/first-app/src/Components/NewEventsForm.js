fc39de79f974e4c2065a1279d157f7272e254a43
Arlen Neylon <aneylon@scramsystems.com>
2023-05-22 17:00:20 -0600

    Work on form controlls.

--- 03-CreateApp/first-app/src/Components/NewEventsForm.js
+++ 03-CreateApp/first-app/src/Components/NewEventsForm.js
@@ -1,16 +1,38 @@
 import "./NewEventsForm.css";
-
+import { useState } from "react";
 export const NewEventsForm = () => {
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventDate, setEventDate] = useState("");
+  const updateEventTitle = (event) => {
+    console.info(event.target.value);
+    setEventTitle(event.target.value);
+  };
+  const updateEventDate = (event) => {
+    console.info(event.target.value);
+    setEventDate(event.target.value);
+  };
+  const resetForm = () => {
+    setEventDate("");
+    setEventTitle("");
+  };
   return (
     <div>
       <form className="new-event-form">
         <label htmlFor="event-title">Event Title:</label>
-        <input id="event-title" />
+        <input
+          id="event-title"
+          value={eventTitle}
+          onChange={updateEventTitle}
+        />
         <label>
           <span>Event Date:</span>
-          <input type="date" />
+          <input type="date" value={eventDate} onChange={updateEventDate} />
         </label>
         <button>Submit</button>
+        <p>
+          - {eventDate} - {eventTitle} -
+        </p>
+        <p onClick={resetForm}>reset</p>
       </form>
     </div>
   );
