07171f0fc1b4f0e090f0c810f6f8f9a8d0b3778a
Arlen Neylon <aneylon@scramsystems.com>
2023-06-06 17:01:17 -0600

    fetch data and style.

--- multipage/src/pages/Home.js
+++ multipage/src/pages/Home.js
@@ -1,12 +1,25 @@
+import { useFetch } from "../hooks/useFetch";
+import "./Home.css";
+
 export default function Home() {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
   return (
-    <div>
-      <h2>Home Page</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias enim
-        nostrum cumque animi ullam sapiente quia molestias, dolorum suscipit,
-        labore odio aut debitis, quasi quidem voluptas? Iusto quaerat a sed.
-      </p>
+    <div className="home">
+      <h1>Home Page</h1>
+      <h2>Articles</h2>
+      {isPending && <div>...Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 }
