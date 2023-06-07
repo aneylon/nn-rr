944e16aa40749e8909e1a51bd0077354b7a2b6ca
Arlen Neylon <aneylon@scramsystems.com>
2023-06-07 15:39:50 -0600

    Add query string stuffs.

--- multipage/src/pages/Contact.js
+++ multipage/src/pages/Contact.js
@@ -1,4 +1,18 @@
+import { useLocation } from "react-router-dom";
+import useQuery from "../hooks/useQuery";
+
 export default function Contact() {
+  const queryString = useLocation().search;
+  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString);
+  console.log(queryParams);
+  const name = queryParams.get("name");
+  console.log(name);
+
+  const query = useQuery();
+  const queryName = query.get("name");
+  console.log({ queryName });
+
   return (
     <div>
       <h2>Contact Page</h2>
