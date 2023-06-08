c8baf7a612d55f4186b219808648b274db61b121
Arlen Neylon <aneylon@scramsystems.com>
2023-06-08 09:10:13 -0600

    Set up routes and pages.

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import {
+  BrowserRouter,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+import Create from "./pages/create/Create";
 
 function App() {
   return (
     <div className="App">
       <h1>Recipes</h1>
+      <BrowserRouter>
+        <nav>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/search">Search</NavLink>
+          <NavLink to="/recipe">Recipe</NavLink>
+          <NavLink to="/create">Create</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route path="/recipe" component={Recipe} />
+          <Route path="/create" component={Create} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ recipe/src/pages/recipe/Recipe.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Recipe.css";
+export default function Recipe() {
+  return <div>Recipe</div>;
+}
