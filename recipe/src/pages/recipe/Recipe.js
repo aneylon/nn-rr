39c950786a630c08a3f3272d2ad03f7f5f78cdf7
Arlen Neylon <aneylon@scramsystems.com>
2023-06-09 18:20:50 -0600

    Get individual items.

--- recipe/src/pages/recipe/Recipe.js
+++ recipe/src/pages/recipe/Recipe.js
@@ -1,5 +1,30 @@
 import React from "react";
 import "./Recipe.css";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const {
+    isLoading,
+    error,
+    data: recipe,
+  } = useFetch(`http://localhost:3000/recipes/${id}`);
+  return (
+    <div className="recipe">
+      {isLoading && <p className="error">...loading...</p>}
+      {error && <p className="loading">{error}</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook. </p>
+          <ul>
+            {recipe.ingredients.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
