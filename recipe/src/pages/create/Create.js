b6b201e232ccc276a4ea1607ff15a01fb189ff6f
aneylon <arlen.m.neylon@gmail.com>
2023-06-10 18:08:17 -0600

    Set up form.

--- recipe/src/pages/create/Create.js
+++ recipe/src/pages/create/Create.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Create.css";
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const submitRecipe = (e) => {
+    e.preventDefault();
+    console.log({ title, method, cookingTime });
+  };
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a new thing</h2>
+      <form onSubmit={submitRecipe}>
+        <label>
+          <span>Title : </span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+        <label>
+          <span>Ingredients : </span>
+          <div className="ingredients">
+            <input type="text" />
+            <button className="btn">add</button>
+          </div>
+        </label>
+        <label>
+          <span>Method : </span>
+          <input
+            type="text"
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Time (minutes) : </span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
