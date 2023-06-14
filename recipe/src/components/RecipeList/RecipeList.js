a45582f6f7af041e18839bbbac499813ec5641c7
Arlen <arlen.m.neylon@gmail.com>
2023-06-14 09:50:19 -0600

    Show different template when nothing returned from search.

--- recipe/src/components/RecipeList/RecipeList.js
+++ recipe/src/components/RecipeList/RecipeList.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function RecipeList({ recipes }) {
+  if (recipes.length === 0)
+    return <div className="error">Nothing to load...</div>;
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
