3165ff3dc0a23bff14053ec0f0aeb7d4d97efcae
Arlen <arlen.m.neylon@gmail.com>
2023-06-19 17:27:54 -0600

    Get individual recipe.

--- recipe/src/pages/recipe/Recipe.js
+++ recipe/src/pages/recipe/Recipe.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Recipe.css";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { useTheme } from "../../hooks/useTheme";
+import { projectFirestore } from "../../firebase/config";
 export default function Recipe() {
   const { id } = useParams();
-  const {
-    isLoading,
-    error,
-    data: recipe,
-  } = useFetch(`http://localhost:3000/recipes/${id}`);
   const { mode } = useTheme();
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: recipe,
+  // } = useFetch(`http://localhost:3000/recipes/${id}`);
+  const [recipe, setRecipe] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        console.log(doc);
+        if (doc.exists) {
+          setIsLoading(false);
+          setRecipe({ id: doc.id, ...doc.data() });
+        } else {
+          setIsLoading(false);
+          setError("Could not find data");
+        }
+      });
+  }, [id]);
+
   return (
     <div className={`recipe ${mode}`}>
       {isLoading && <p className="error">...loading...</p>}
