bd0d9dbeae0676e54ee512a4379f10fb779359c0
Arlen <arlen.m.neylon@gmail.com>
2023-06-21 10:45:49 -0600

    Set up routes.

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Stuffs</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
