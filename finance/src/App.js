b26be090e7092fa413d0f582eec29ca22a481520
Arlen <arlen.m.neylon@gmail.com>
2023-06-22 08:40:14 -0600

    Add navbar.

--- finance/src/App.js
+++ finance/src/App.js
@@ -3,12 +3,13 @@ import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Stuffs</h1>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
---
+++ finance/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>
+          <Link to="/">Things</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
