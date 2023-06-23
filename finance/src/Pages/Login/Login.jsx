60d0e07c3ab1b47aa619c0a5292ec7a7732fbb9d
Arlen <arlen.m.neylon@gmail.com>
2023-06-22 18:56:50 -0600

    Add login page.

--- finance/src/Pages/Login/Login.jsx
+++ finance/src/Pages/Login/Login.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form className={styles["login-form"]} onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   );
 }
