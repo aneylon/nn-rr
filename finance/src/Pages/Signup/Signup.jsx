dc05e64a31425cf39e5f510924e232c8949ce194
Arlen <arlen.m.neylon@gmail.com>
2023-06-23 10:10:36 -0600

    Set up signup page.

--- finance/src/Pages/Signup/Signup.jsx
+++ finance/src/Pages/Signup/Signup.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
 import styles from "./Signup.module.css";
 
 export default function Signup() {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log({ displayName, email, password });
+  };
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <form className={styles["signup-form"]} onSubmit={handleLogin}>
+      <h2>Signup</h2>
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => {
+            setDisplayName(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
   );
 }
