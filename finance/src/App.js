aca7c7556256d754380689edda5b574b64b88d7a
Arlen <arlen.m.neylon@gmail.com>
2023-06-28 16:57:39 -0600

    Set up route guards and wait for auth to show content.

--- finance/src/App.js
+++ finance/src/App.js
@@ -4,19 +4,32 @@ import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 export default function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {user && <Home />}
+              {!user && <Redirect to="/login" />}
+            </Route>
+            <Route path="/login">
+              {!user && <Login />}
+              {user && <Redirect to="/" />}
+            </Route>
+            <Route path="/signup">
+              {!user && <Signup />}
+              {user && <Redirect to="/" />}
+            </Route>
+            <Redirect from="*" to="/" />
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
