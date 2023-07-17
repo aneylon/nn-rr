08a06b715cdadc0d09e9077fc618e8526bfb7ccb
Arlen <arlen.m.neylon@gmail.com>
2023-07-17 13:45:42 -0600

    Add pages.

--- managr/src/App.js
+++ managr/src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Create from "./pages/create/Create";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Dashboard from "./pages/dashboard/Dashboard";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Managr</h1>
+      <BrowserRouter>
+        <h1>Managr</h1>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={Project} />
+            <Route path="/create" component={Create} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
