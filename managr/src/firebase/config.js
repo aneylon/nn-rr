32dfe74ceb236156bea8e6468f6b750f77da5229
Arlen <arlen.m.neylon@gmail.com>
2023-07-18 16:04:15 -0600

    setup to upload image with signup.

--- managr/src/firebase/config.js
+++ managr/src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwks2vI6ukBiKC0_bxClxcAsY1GJ-TY4c",
@@ -15,7 +16,8 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectAuth, projectFirestore, timestamp };
+export { projectAuth, projectFirestore, projectStorage, timestamp };
